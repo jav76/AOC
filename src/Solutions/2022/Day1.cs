8b7bfe62dc84684fd0ade9ebaa36b42d1114139c
Jaret Varn <jav26122@gmail.com>
2023-11-24 17:31:07 -0500

    2022 day1 solution

---
+++ src/Solutions/2022/Day1.cs
@@ -0,0 +1,53 @@
+﻿
+
+namespace AOC.Solutions._2022
+{
+    internal class Day1 : SolutionBase, ISolution
+    {
+
+        private List<int> calorieSums= new List<int>();
+
+        private List<int> GetCalorieSums(List<string> inputLines)
+        {
+            List<int> calorieSums = new List<int>();
+            int currentSum = 0;
+
+            foreach (string line in inputLines)
+            {
+                if (line.Length == 0)
+                {
+                    calorieSums.Add(currentSum);
+                    currentSum = 0;
+                }
+
+                int currentCalories = 0;
+                if (int.TryParse(line, out currentCalories))
+                {
+                    currentSum+= currentCalories;
+                }
+            }
+
+            return calorieSums;
+        }
+
+        public override string Part1Solution()
+        {
+            return GetCalorieSums(InputLines)
+                .OrderBy(x => x)
+                .Reverse()
+                .First()
+                .ToString();
+        }
+
+        public override string Part2Solution()
+        {
+            List<int> top3 = GetCalorieSums(InputLines)
+                .OrderBy(x => x)
+                .Reverse()
+                .Take(3)
+                .ToList();
+
+            return top3.Sum().ToString();
+        }
+    }
+}
